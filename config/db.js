18bf5b171a821b4eacc00315d197af46990871b6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-18 03:30:48 +0100

    fix: refactor mongodb connection

---
+++ config/db.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config({ path: './config.env' });
+
+const devEnv = process.env.NODE_ENV !== 'production';
+const { DATABASE, DATABASE_LOCAL, DATABASE_PASSWORD } = process.env;
+
+// database local
+const dbLocal = DATABASE_LOCAL;
+
+// atlas mongo uri
+const mongoURI = DATABASE.replace('<PASSWORD>', DATABASE_PASSWORD);
+
+const sb = devEnv ? dbLocal : mongoURI;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(db);
+    console.log(`Connected to MongoDB → ${conn.connection.port}`.gray.bold);
+  } catch (err) {
+    throw err;
+  }
+};
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected'.yellow.bold);
+});
+
+export default connectDB;
