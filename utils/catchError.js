4b8b1d4731480c75e8adf94138199510b3cef3c0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-18 03:55:31 +0100

    fix: convert to es6 module

--- utils/catchError.js
+++ utils/catchError.js
@@ -1,5 +1,7 @@
-module.exports = (fn) => {
+const catchError = (fn) => {
   return (req, res, next) => {
     return fn(req, res, next).catch((err) => next(err));
   };
 };
+
+export default catchError;
