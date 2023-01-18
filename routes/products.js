34f77b592bd522bd52272bdf17694344698c96e6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-18 03:53:29 +0100

    fix: modify

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import productController from '../controllers/productController.js';
+import * as productController from '../controllers/productController.js';
 
 const router = express.Router();
 
