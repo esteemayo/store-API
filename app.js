78a6e1293eb3d9bfd9ac0a071aee70ed02f53d59
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-23 00:59:28 +0100

    fix: modify

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import mongoSanitize from 'express-mongo-sanitize';
+import cors from 'cors';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
 import morgan from 'morgan';
